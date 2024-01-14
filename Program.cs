b1328b2967213611a82adaf9a6a4df077be64333
Clawiatura <154603004+Clawiatura@users.noreply.github.com>
2024-01-14 15:23:50 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Moscow = 13015126;
+int Spb = 5598486;
+int inh = Moscow % 1000000 / 1000;
+int nih = Spb % 1000000 / 1000;
+int finish = nih - inh;
+Console.WriteLine($"На {finish} тысяч меньше в Москве");
